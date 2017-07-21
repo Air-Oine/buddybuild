d450ebc63f428becd91b289492396b3202c8a2d8
Erwan Masson <airone.masson@gmail.com>
2017-07-21 12:31:11 +0200

    Corection eslint

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -49,7 +49,7 @@ export default class HomeScreen extends Component {
     this.navigate(STATE_SCENE_NAME);
   }
 
-  alerte() {
+  static alerte() {
     Alert.alert(
       'Alert Title',
       'My Alert Msg',
